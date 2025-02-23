22c570ad2332fdca457cce88884cc94cb8a590a2
Tyler Wilcox <tyler@wilcoxflp.com>
2025-02-23 12:28:44 -0500

    Better test handling

--- lib/src/main/java/org/fairportrobotics/frc/posty/TestableSubsystem.java
+++ lib/src/main/java/org/fairportrobotics/frc/posty/TestableSubsystem.java
@@ -4,19 +4,45 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import java.util.ArrayList;
 
+import org.ejml.simple.UnsupportedOperation;
+
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 
 public abstract class TestableSubsystem extends SubsystemBase{
 
   @FunctionalInterface
-  public interface POSTTest{
+  public interface POSTTestRunner{
     boolean postTest();
   }
 
   @FunctionalInterface
-  public interface BITTest {
+  public interface BITTestRunner {
     boolean bitTest();
+  }
+
+  private class POSTTest{
+    String name;
+    POSTTestRunner testRunner;
+    Alert testAlert;
+
+    public POSTTest(String testName, POSTTestRunner testRunner, Alert testAlert){
+      this.name = testName;
+      this.testRunner = testRunner;
+      this.testAlert = testAlert;
+    }
+  }
+
+  private class BITTest{
+    String name;
+    BITTestRunner testRunner;
+    Alert testAlert;
+
+    public BITTest(String testName, BITTestRunner testRunner, Alert testAlert){
+      this.name = testName;
+      this.testRunner = testRunner;
+      this.testAlert = testAlert;
+    }
   }
 
   private ArrayList<POSTTest> m_POSTTests;
@@ -31,25 +57,32 @@ public abstract class TestableSubsystem extends SubsystemBase{
   }
 
   /**
-   * 
+   * This function is called before all POST tests are run. Put any setup logic in here.
    * @return
    */
-  public abstract boolean onPowerOnTest();
+  public boolean onPowerOnTest() { return true; }
 
   /**
-   * 
+   * This function is called before all BIT tests are run. Put any setup logic in here.
    * @return
    */
-  public abstract boolean onBuiltInTest();
+  public boolean onBuiltInTest(){ return true; }
 
   public void runPOST(){
 
     Boolean postFailed = false;
 
-    postFailed |= onPowerOnTest();
+    try{
+        postFailed |= !onPowerOnTest();
+    }catch(UnsupportedOperationException ex){
+        // Default exception when function is overriden
+    }
 
     for(POSTTest pTest : m_POSTTests){
-      postFailed |= pTest.postTest();
+      boolean testPassed = pTest.testRunner.postTest();
+      if(!testPassed)
+        pTest.testAlert.set(true);
+      postFailed |= !testPassed;
     }
 
     if(postFailed){
@@ -64,8 +97,17 @@ public abstract class TestableSubsystem extends SubsystemBase{
 
     Boolean bitFailed = false;
 
+    try{
+        bitFailed |= !onBuiltInTest();
+    }catch(UnsupportedOperationException ex){
+        // Default exception when function is overriden
+    }
+
     for(BITTest bTest : m_BITTests){
-      bitFailed |= bTest.bitTest();
+      boolean testPassed = bTest.testRunner.bitTest();
+      if(!testPassed)
+        bTest.testAlert.set(true);
+      bitFailed |= !testPassed;
     }
 
     if(bitFailed){
@@ -77,19 +119,53 @@ public abstract class TestableSubsystem extends SubsystemBase{
   }
 
   /**
-   * 
-   * @param pTest
+   * Register a Power on Self Test(POST) test.
+   *
+   * This test will be run on every power up of the robot.
+   *
+   * @param testName The name of this test
+   * @param postTest The runnable that defines the test. Should return true if the test passes, false if the test fails
+   * @param alertType The type of alert to display when the test fails
    */
-  protected void registerPOSTTest(POSTTest pTest){
-    m_POSTTests.add(pTest);
+  protected void registerPOSTTest(String testName, POSTTestRunner postTest, AlertType alertType){
+    m_POSTTests.add(new POSTTest(testName, postTest, new Alert(getName() + ":" + testName, alertType)));
   }
 
   /**
-   * 
-   * @param bTest
+   * Register a Power on Self Test(POST) test.
+   *
+   * This test will be run on every power up of the robot.
+   *
+   * @param testName The name of this test
+   * @param postTest The runnable that defines the test. Should return true if the test passes, false if the test fails
    */
-  protected void registerBITTest(BITTest bTest){
-    m_BITTests.add(bTest);
+  protected void registerPOSTTest(String testName, POSTTestRunner postTest){
+    m_POSTTests.add(new POSTTest(testName, postTest, new Alert(getName() + ":" + testName, AlertType.kError)));
+  }
+
+  /**
+   * Register a Built in Test(BIT) test.
+   *
+   * This test will be run when the robot is enabled in Test mode.
+   *
+   * @param testName The name of this test
+   * @param bitTest The runnable that defines the test. Should return true if the test passes, false if the test fails
+   * @param alertType The type of alert to display when the test fails
+   */
+  protected void registerBITTest(String testName, BITTestRunner bitTest, AlertType alertType){
+    m_BITTests.add(new BITTest(testName, bitTest, new Alert(getName() + ":" + testName, alertType)));
+  }
+
+  /**
+   * Register a Built in Test(BIT) test.
+   *
+   * This test will be run when the robot is enabled in Test mode.
+   *
+   * @param testName The name of this test
+   * @param bitTest The runnable that defines the test. Should return true if the test passes, false if the test fails
+   */
+  protected void registerBITTest(String testName, BITTestRunner bitTest){
+    m_BITTests.add(new BITTest(testName, bitTest, new Alert(getName() + ":" + testName, AlertType.kError)));
   }
 
   protected void throwError(String msg){
