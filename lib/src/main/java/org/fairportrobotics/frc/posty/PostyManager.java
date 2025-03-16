eef032570507ab48ed56d58098a9593ce51a47bd
Tyler Wilcox <tyler@tywilly.com>
2025-03-16 16:15:54 -0400

    Rework

--- lib/src/main/java/org/fairportrobotics/frc/posty/PostyManager.java
+++ lib/src/main/java/org/fairportrobotics/frc/posty/PostyManager.java
@@ -1,31 +1,55 @@
 package org.fairportrobotics.frc.posty;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.fairportrobotics.frc.posty.exception.PostyTimeoutException;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class PostyManager {
 
   private Thread postTestRunnerThread;
   private Thread bitTestRunnerThread;
 
-  private ArrayList<TestableSubsystem> m_registeredSubsystem;
+  private HashMap<String, ArrayList<Command>> postCommands;
+  private HashMap<String, ArrayList<Command>> bitCommands;
 
   public PostyManager(){
-    m_registeredSubsystem = new ArrayList<TestableSubsystem>();
+    postCommands = new HashMap<>();
+    bitCommands = new HashMap<>();
   }
 
-  public void registerSubsystem(TestableSubsystem subsystem){
-    m_registeredSubsystem.add(subsystem);
+  public boolean addPOST(String subsystemName, Command postCommand){
+    if(!postCommands.containsKey(subsystemName)){
+      postCommands.put(subsystemName, new ArrayList<>());
+    }
+
+    return postCommands.get(subsystemName).add(postCommand);
+  }
+
+  public boolean addBIT(String subsystemName, Command bitCommand){
+    if(!bitCommands.containsKey(subsystemName)){
+      bitCommands.put(subsystemName, new ArrayList<>());
+    }
+
+    return bitCommands.get(subsystemName).add(bitCommand);
   }
 
   public void runAllPOSTs(){
   
     postTestRunnerThread = new Thread(() -> {
-      for(TestableSubsystem subsystem : m_registeredSubsystem){
-        try{
-          subsystem.runPOST();
-        }catch(PostyTimeoutException ex){
+      for(ArrayList<Command> subsystemCommands : postCommands.values()){
+        for(Command command : subsystemCommands){
+
+          command.initialize();
+
+          do{
+            command.execute();
+          }while(!command.isFinished());
+
+          command.end(false);
 
         }
       }
@@ -37,11 +61,17 @@ public class PostyManager {
 
   public void runAllBITs(){
     bitTestRunnerThread = new Thread(() -> {
-      for(TestableSubsystem subsystem : m_registeredSubsystem){
-        try{
-          subsystem.runBIT();
-        }catch(PostyTimeoutException ex){
-          ex.printStackTrace();
+      for(ArrayList<Command> subsystemCommands : bitCommands.values()){
+        for(Command command : subsystemCommands){
+
+          command.initialize();
+
+          do{
+            command.execute();
+          }while(!command.isFinished());
+
+          command.end(false);
+
         }
       }
     });
